f9cbf2d00c11eb087ffc6964e406565eccb64cc2
vcarehuman <60559538+vcarehuman@users.noreply.github.com>
2021-08-29 05:48:57 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -120,6 +120,38 @@ def main(img_pattern: str,
     else:
         process_video(pairs, predictor, out_dir)
 
+def unblur(img_pattern: str,
+         mask_pattern: Optional[str] = None,
+         weights_path='fpn_inception.h5',
+         out_dir='/mydrive/images/test/resultsUnBlur/',
+         side_by_side: bool = False,
+         video: bool = False):
+    def sorted_glob(pattern):
+        return sorted(glob(pattern))
+
+    imgs = sorted_glob(img_pattern)
+    masks = sorted_glob(mask_pattern) if mask_pattern is not None else [None for _ in imgs]
+    pairs = zip(imgs, masks)
+    names = sorted([os.path.basename(x) for x in glob(img_pattern)])
+    predictor = Predictor(weights_path=weights_path)
+
+    os.makedirs(out_dir, exist_ok=True)
+    if not video:
+        for name, pair in tqdm(zip(names, pairs), total=len(names)):
+            f_img, f_mask = pair
+            img, mask = map(cv2.imread, (f_img, f_mask))
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+            pred = predictor(img, mask)
+            if side_by_side:
+                pred = np.hstack((img, pred))
+            pred = cv2.cvtColor(pred, cv2.COLOR_RGB2BGR)
+            print("writing file at ",out_dir, name)
+            cv2.imwrite(os.path.join(out_dir, name),
+                        pred)
+    else:
+        process_video(pairs, predictor, out_dir)
+
 
 if __name__ == '__main__':
     Fire(main)
